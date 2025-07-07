8851113a5c9c5a150341d44f5a6fe59689951926
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2025-07-07 20:45:42 -0300

    Update imm_Gen.sv
    testando uma coisa

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -24,7 +24,7 @@ module imm_Gen (
         1'b0
       };
       7'b0010011: //I type
-      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+          Imm_out = {inst_code[31] ? 20'b1 : 20'b0, inst_code[31:20]};
 
       default: Imm_out = {32'b0};
 
