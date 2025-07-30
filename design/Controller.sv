e9a7ff6ac3971c63136736f2a8252a454cfc19ee
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2025-07-30 16:03:46 -0300

    finish jal & jalr

--- design/Controller.sv
+++ design/Controller.sv
@@ -16,23 +16,29 @@ module Controller (
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic Branch,  //0: branch is not taken; 1: branch is taken
+    output logic JalrSel
 );
 
-    logic [6:0] R_TYPE, I_TYPE, LW, SW, BR;
+    logic [6:0] R_TYPE, I_TYPE, LW, SW, BR, JAL, JALR;
 
   assign R_TYPE = 7'b0110011;  //add,and,xor,or, (...)
   assign I_TYPE = 7'b0010011;    //imediatas
   assign LW = 7'b0000011;  //lw, lb, lh, lbu
   assign SW = 7'b0100011;  //sw, sb, sh
   assign BR = 7'b1100011;  //beq
+  assign JAL = 7'b1101111;
+  assign JALR = 7'b1100111;
 
   assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == JAL || Opcode == JALR);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR);
   assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE);
-  assign Branch = (Opcode == BR);
+  assign Branch = (Opcode == BR || Opcode == JAL);
+  assign JalrSel = (Opcode == JAL || Opcode == JALR);
 endmodule
+
+
